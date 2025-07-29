f40b7aabd6d95d50015cf954329473927ea09f12
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-07-30 04:00:44 +0600

    basic nodejs route

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const http = require('http');
+const data=[
+  {id:1,name:"sabbir",email:"sabbir@gmail.com",time:"2023-10-01T12:00:00Z"},
+  {id:2,name:"john",email:"habu@gmail.com",time:"2023-10-01T12:00:00Z"},
+  {id:3,name:"doe",email:"nasu@gmail.com",time:"2023-10-01T12:00:00Z"},
+  {id:4,name:"jane",email:"kabu@gmail.com",time:"2023-10-01T12:00:00Z"},
+]
 
 const server=http.createServer((req,res)=>{
   if(req.url==='/hello' && req.method==='GET'){
-    res.end('Hello endpoint was hit');
+    res.writeHead(200,{
+      // "content-type": "text/plain",
+      "content-type": "application/json",
+      // "email":"sabbir@gmail.com"
+    })
+    res.end(JSON.stringify(data));
   }
   else if(req.url==='/hello/new' && req.method==='POST'){
     res.end('Bye endpoint was hit');
